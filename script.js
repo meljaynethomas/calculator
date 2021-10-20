917e72f21bae974018df4e5f766a88b5f48ceb22
Mel Thomas <meljaynethomas@gmail.com>
2021-10-20 09:35:01 +0100

    cleaned up code in script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-//assign a value to each button
-//make that value display in <p> tag as each button is pressed
-//keep this value in the <p> tag and add others to it
-//make the numbers/symbols within the <p> tag into a sum
-//calculate the sum and display the answer
-
-let buttonOne = document.getElementById("buttonOne");
-let buttonTwo = document.getElementById("buttonTwo");
-let buttonThree = document.getElementById("buttonThree");
-let buttonFour = document.getElementById("buttonFour");
-let buttonFive = document.getElementById("buttonFive");
-let buttonSix = document.getElementById("buttonSix");
-let buttonSeven = document.getElementById("buttonSeven");
-let buttonEight = document.getElementById("buttonEight");
-let buttonNine = document.getElementById("buttonNine");
-let clearButton = document.getElementById("clearButton");
-let addButton = document.getElementById("addButton");
-let subtractButton = document.getElementById("subtractButton");
-let divideButton = document.getElementById("divideButton");
-let multiplyButton = document.getElementById("multiplyButton");
-let equalsButton = document.getElementById("equalsButton");
-
-
-
 const getNumber = (event) => {
   let value = event.target.textContent;
   const parsedValue = parseInt(value);
@@ -39,41 +15,41 @@ const calculateAnswer = (event) => {
   let sum = document.getElementById("calculatorDisplay").textContent;
   console.log(sum);
 
-      for (let index = 0; index <= sum.length; index++) {
-        if (sum.charAt(index) == '+') {
-          let firstNumber = sum.substring(0, index);
-          let parsedFirstNumber = parseInt(firstNumber);
-          let secondNumber = sum.substring(index+1);
-          let parsedSecondNumber = parseInt(secondNumber);
-          let total = parsedFirstNumber+parsedSecondNumber;
-          document.getElementById('calculatorDisplay').textContent = " ";
-          document.getElementById('calculatorDisplay').textContent = total;
-        } else if (sum.charAt(index) == '-') {
-          let firstNumber = sum.substring(0, index);
-          let parsedFirstNumber = parseInt(firstNumber);
-          let secondNumber = sum.substring(index+1);
-          let parsedSecondNumber = parseInt(secondNumber);
-          let total = parsedFirstNumber-parsedSecondNumber;
-          document.getElementById('calculatorDisplay').textContent = " ";
-          document.getElementById('calculatorDisplay').textContent = total;
-        } else if (sum.charAt(index) == '*') {
-          let firstNumber = sum.substring(0, index);
-          let parsedFirstNumber = parseInt(firstNumber);
-          let secondNumber = sum.substring(index+1);
-          let parsedSecondNumber = parseInt(secondNumber);
-          let total = parsedFirstNumber*parsedSecondNumber;
-          document.getElementById('calculatorDisplay').textContent = " ";
-          document.getElementById('calculatorDisplay').textContent = total;
-        } else if (sum.charAt(index) == '/') {
-          let firstNumber = sum.substring(0, index);
-          let parsedFirstNumber = parseInt(firstNumber);
-          let secondNumber = sum.substring(index+1);
-          let parsedSecondNumber = parseInt(secondNumber);
-          let total = parsedFirstNumber/parsedSecondNumber;
-          document.getElementById('calculatorDisplay').textContent = " ";
-          document.getElementById('calculatorDisplay').textContent = total;
-        }
+    for (let index = 0; index <= sum.length; index++) {
+      if (sum.charAt(index) == '+') {
+        let firstNumber = sum.substring(0, index);
+        let parsedFirstNumber = parseInt(firstNumber);
+        let secondNumber = sum.substring(index+1);
+        let parsedSecondNumber = parseInt(secondNumber);
+        let total = parsedFirstNumber+parsedSecondNumber;
+        document.getElementById('calculatorDisplay').textContent = " ";
+        document.getElementById('calculatorDisplay').textContent = total;
+      } else if (sum.charAt(index) == '-') {
+        let firstNumber = sum.substring(0, index);
+        let parsedFirstNumber = parseInt(firstNumber);
+        let secondNumber = sum.substring(index+1);
+        let parsedSecondNumber = parseInt(secondNumber);
+        let total = parsedFirstNumber-parsedSecondNumber;
+        document.getElementById('calculatorDisplay').textContent = " ";
+        document.getElementById('calculatorDisplay').textContent = total;
+      } else if (sum.charAt(index) == '*') {
+        let firstNumber = sum.substring(0, index);
+        let parsedFirstNumber = parseInt(firstNumber);
+        let secondNumber = sum.substring(index+1);
+        let parsedSecondNumber = parseInt(secondNumber);
+        let total = parsedFirstNumber*parsedSecondNumber;
+        document.getElementById('calculatorDisplay').textContent = " ";
+        document.getElementById('calculatorDisplay').textContent = total;
+      } else if (sum.charAt(index) == '/') {
+        let firstNumber = sum.substring(0, index);
+        let parsedFirstNumber = parseInt(firstNumber);
+        let secondNumber = sum.substring(index+1);
+        let parsedSecondNumber = parseInt(secondNumber);
+        let total = parsedFirstNumber/parsedSecondNumber;
+        document.getElementById('calculatorDisplay').textContent = " ";
+        document.getElementById('calculatorDisplay').textContent = total;
       }
+    }
 }
 
 
