b63820d7ed3ff54b43a34e285eb4223d89c2cba0
Mel Thomas <meljaynethomas@gmail.com>
2021-10-21 12:08:29 +0100

    changed calculator display back to id from class, as changing it caused a lot of issues in my javascript file

--- script.js
+++ script.js
@@ -15,41 +15,41 @@ const calculateAnswer = (event) => {
   let sum = document.getElementById("calculatorDisplay").textContent;
   console.log(sum);
 
-    for (let index = 0; index <= sum.length; index++) {
-      if (sum.charAt(index) == '+') {
-        let firstNumber = sum.substring(0, index);
-        let parsedFirstNumber = parseInt(firstNumber);
-        let secondNumber = sum.substring(index+1);
-        let parsedSecondNumber = parseInt(secondNumber);
-        let total = parsedFirstNumber+parsedSecondNumber;
-        document.getElementById('calculatorDisplay').textContent = " ";
-        document.getElementById('calculatorDisplay').textContent = total;
-      } else if (sum.charAt(index) == '-') {
-        let firstNumber = sum.substring(0, index);
-        let parsedFirstNumber = parseInt(firstNumber);
-        let secondNumber = sum.substring(index+1);
-        let parsedSecondNumber = parseInt(secondNumber);
-        let total = parsedFirstNumber-parsedSecondNumber;
-        document.getElementById('calculatorDisplay').textContent = " ";
-        document.getElementById('calculatorDisplay').textContent = total;
-      } else if (sum.charAt(index) == '*') {
-        let firstNumber = sum.substring(0, index);
-        let parsedFirstNumber = parseInt(firstNumber);
-        let secondNumber = sum.substring(index+1);
-        let parsedSecondNumber = parseInt(secondNumber);
-        let total = parsedFirstNumber*parsedSecondNumber;
-        document.getElementById('calculatorDisplay').textContent = " ";
-        document.getElementById('calculatorDisplay').textContent = total;
-      } else if (sum.charAt(index) == '/') {
-        let firstNumber = sum.substring(0, index);
-        let parsedFirstNumber = parseInt(firstNumber);
-        let secondNumber = sum.substring(index+1);
-        let parsedSecondNumber = parseInt(secondNumber);
-        let total = parsedFirstNumber/parsedSecondNumber;
-        document.getElementById('calculatorDisplay').textContent = " ";
-        document.getElementById('calculatorDisplay').textContent = total;
+      for (let index = 0; index <= sum.length; index++) {
+        if (sum.charAt(index) == '+') {
+          let firstNumber = sum.substring(0, index);
+          let parsedFirstNumber = parseInt(firstNumber);
+          let secondNumber = sum.substring(index+1);
+          let parsedSecondNumber = parseInt(secondNumber);
+          let total = parsedFirstNumber+parsedSecondNumber;
+          document.getElementById('calculatorDisplay').textContent = " ";
+          document.getElementById('calculatorDisplay').textContent = total;
+        } else if (sum.charAt(index) == '-') {
+          let firstNumber = sum.substring(0, index);
+          let parsedFirstNumber = parseInt(firstNumber);
+          let secondNumber = sum.substring(index+1);
+          let parsedSecondNumber = parseInt(secondNumber);
+          let total = parsedFirstNumber-parsedSecondNumber;
+          document.getElementById('calculatorDisplay').textContent = " ";
+          document.getElementById('calculatorDisplay').textContent = total;
+        } else if (sum.charAt(index) == '*') {
+          let firstNumber = sum.substring(0, index);
+          let parsedFirstNumber = parseInt(firstNumber);
+          let secondNumber = sum.substring(index+1);
+          let parsedSecondNumber = parseInt(secondNumber);
+          let total = parsedFirstNumber*parsedSecondNumber;
+          document.getElementById('calculatorDisplay').textContent = " ";
+          document.getElementById('calculatorDisplay').textContent = total;
+        } else if (sum.charAt(index) == '/') {
+          let firstNumber = sum.substring(0, index);
+          let parsedFirstNumber = parseInt(firstNumber);
+          let secondNumber = sum.substring(index+1);
+          let parsedSecondNumber = parseInt(secondNumber);
+          let total = parsedFirstNumber/parsedSecondNumber;
+          document.getElementById('calculatorDisplay').textContent = " ";
+          document.getElementById('calculatorDisplay').textContent = total;
+        }
       }
-    }
 }
 
 
